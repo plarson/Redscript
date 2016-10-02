e7b2f5b448749b796b5f4c4cff5d11a62bb30eca
Phil Larson <phil.larson@icloud.com>
2016-10-02 05:54:12 -0700

    Upgrade Redbird dependency

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ var package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 1),
-        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0, minor: 10)
+        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 0, minor: 11)
         ]
 )
 
